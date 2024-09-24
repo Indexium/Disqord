b6225e1e149deb8739b90f827615993e880144a3
Ganan <26074413+Junko-Ganan@users.noreply.github.com>
2024-09-24 14:22:54 -0400

    Update InteractivityMenuClientExtensions.cs

--- src/Disqord.Extensions.Interactivity/Extensions/Menus/InteractivityMenuClientExtensions.cs
+++ src/Disqord.Extensions.Interactivity/Extensions/Menus/InteractivityMenuClientExtensions.cs
@@ -10,19 +10,19 @@ public static class InteractivityMenuClientExtensions
 {
     /// <inheritdoc cref="InteractivityExtension.StartMenuAsync"/>
     public static Task StartMenuAsync(this DiscordClientBase client,
-        Snowflake channelId, MenuBase menu,
+        Snowflake guildId, Snowflake channelId, MenuBase menu,
         TimeSpan timeout = default, CancellationToken cancellationToken = default)
     {
         var extension = client.GetInteractivity();
-        return extension.StartMenuAsync(channelId, menu, timeout, cancellationToken);
+        return extension.StartMenuAsync(guildId, channelId, menu, timeout, cancellationToken);
     }
 
     /// <inheritdoc cref="InteractivityExtension.RunMenuAsync"/>
     public static Task RunMenuAsync(this DiscordClientBase client,
-        Snowflake channelId, MenuBase menu,
+        Snowflake guildId, Snowflake channelId, MenuBase menu,
         TimeSpan timeout = default, CancellationToken cancellationToken = default)
     {
         var extension = client.GetInteractivity();
-        return extension.RunMenuAsync(channelId, menu, timeout, cancellationToken);
+        return extension.RunMenuAsync(guildId, channelId, menu, timeout, cancellationToken);
     }
 }
